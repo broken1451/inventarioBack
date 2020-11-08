3d65f970cf9e2999eb161a67916a7cfa937acedc
Adrian <adrianbravo145@gmail.com>
2020-11-07 22:05:08 -0300

    upload refactor

--- utils/subirImg.ts
+++ utils/subirImg.ts
@@ -2,34 +2,23 @@ import { Usuario } from "../models/userModel";
 import fileSystem from "fs";
 import { Response } from "express";
 
-export const subirImg = async (
-  tipoImagen: string,
-  id: string,
-  nombreArchivo: string,
-  res?: Response
-) => {
+
+const borrarImg = (path: any) => {
+    if (fileSystem.existsSync(path)) {
+        fileSystem.unlinkSync(path)
+      }
+}
+
+export const subirImg = async (tipoImagen: string, id: string, nombreArchivo: string, res?: Response) => {
+
+  let pathViejo = ''
   switch (tipoImagen) {
     case "usuario":
       try {
         const user = await Usuario.findById(id).exec();
         if (user) {
-          const pathViejo = "./uploads/usuario/" + user.img; // pathViejo de la imagen si el usuario ya tiene una guardada
-          if (fileSystem.existsSync(pathViejo)) {
-            // si existe elimina la imagen anterior
-            // console.log(fileSystem.existsSync(pathViejo))
-            // console.log(pathViejo)
-            fileSystem.unlinkSync(pathViejo)
-            // fileSystem.unlink(pathViejo, (err) => {
-            //   if (err) {
-            //     return res?.status(500).json({
-            //       ok: false,
-            //       mensaje: "Error en path",
-            //       errors: err,
-            //     });
-            //   }
-            // });
-          }
-
+          pathViejo = "./uploads/usuario/" + user.img; // pathViejo de la imagen si el usuario ya tiene una guardada
+          borrarImg(pathViejo)
           user.img = nombreArchivo;
           await user.save();
           user.password = ":)";
