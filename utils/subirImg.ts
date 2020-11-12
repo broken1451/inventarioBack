9a5873b869d7bec2fa3e56b87f354d73429c1f73
Adrian <adrianbravo145@gmail.com>
2020-11-11 23:09:17 -0300

    module others  ready

--- utils/subirImg.ts
+++ utils/subirImg.ts
@@ -4,26 +4,25 @@ import { Memorias } from "../models/memoriaModel";
 import { Otros } from "../models/otrosModel";
 import fileSystem from "fs";
 import { Response } from "express";
-import { Imgs } from '../models/imgs';
+import { Imgs } from "../models/imgs";
 
 const borrarImg = (path: any) => {
-    if (fileSystem.existsSync(path)) {
-        fileSystem.unlinkSync(path)
-      }
-}
+  if (fileSystem.existsSync(path)) {
+    fileSystem.unlinkSync(path);
+  }
+};
 
-export const subirImg = async (tipoImagen: string, id: string, nombreArchivo: string, res?: Response) => {
-
+export const subirImg = async ( tipoImagen: string, id: string, nombreArchivo: string, res?: Response ) => {
   try {
-    let pathViejo = ''
+    let pathViejo = "";
     switch (tipoImagen) {
       case "usuario":
         try {
           const user = await Usuario.findById(id).exec();
           if (user) {
-              if (!pathViejo && user.img == '') {
+            if (!pathViejo && user.img == "") {
               pathViejo = "../uploads/usuario/" + user.img;
-              borrarImg(pathViejo)
+              borrarImg(pathViejo);
               user.img = nombreArchivo;
               await user.save();
               user.password = ":)";
@@ -32,9 +31,9 @@ export const subirImg = async (tipoImagen: string, id: string, nombreArchivo: st
                 mensaje: "user actualizado con exito",
                 user,
               });
-            }else {
-              pathViejo = "./uploads/usuario/" + user.img;  // pathViejo de la imagen si el usuario ya tiene una guardada
-              borrarImg(pathViejo)
+            } else {
+              pathViejo = "./uploads/usuario/" + user.img; // pathViejo de la imagen si el usuario ya tiene una guardada
+              borrarImg(pathViejo);
               user.img = nombreArchivo;
               await user.save();
               user.password = ":)";
@@ -53,135 +52,132 @@ export const subirImg = async (tipoImagen: string, id: string, nombreArchivo: st
           });
         }
         break;
-        case "pc":
-          try {
-            const pc = await Pc.findById(id).exec();
-            if (pc) {
-                if (!pathViejo && pc.img == '') {
-                pathViejo = "../uploads/pc/" + pc.img;
-                borrarImg(pathViejo)
-                pc.img = nombreArchivo;
-                await pc.save();
-                return res?.status(200).json({
-                  ok: true,
-                  mensaje: "Pc actualizado con exito",
-                  pc,
-                });
-              }else {
-                pathViejo = "./uploads/pc/" + pc.img;  // pathViejo de la imagen si el usuario ya tiene una guardada
-                borrarImg(pathViejo)
-                pc.img = nombreArchivo;
-                await pc.save();
-                return res?.status(200).json({
-                  ok: true,
-                  mensaje: "Pc actualizado con exito",
-                  pc,
-                });
-              }
+      case "pc":
+        try {
+          const pc = await Pc.findById(id).exec();
+          if (pc) {
+            if (!pathViejo && pc.img == "") {
+              pathViejo = "../uploads/pc/" + pc.img;
+              borrarImg(pathViejo);
+              pc.img = nombreArchivo;
+              await pc.save();
+              return res?.status(200).json({
+                ok: true,
+                mensaje: "Pc actualizado con exito",
+                pc,
+              });
+            } else {
+              pathViejo = "./uploads/pc/" + pc.img; // pathViejo de la imagen si el usuario ya tiene una guardada
+              borrarImg(pathViejo);
+              pc.img = nombreArchivo;
+              await pc.save();
+              return res?.status(200).json({
+                ok: true,
+                mensaje: "Pc actualizado con exito",
+                pc,
+              });
             }
-          } catch (error) {
-            // console.log({ error });
-            return res?.json({
-              ok: false,
-              mensaje: `No existe un pc con id ${id}`,
-            });
           }
+        } catch (error) {
+          // console.log({ error });
+          return res?.json({
+            ok: false,
+            mensaje: `No existe un pc con id ${id}`,
+          });
+        }
         break;
-        case "memoria":
-          try {
-            const memoria = await Memorias.findById(id).exec();
-            if (memoria) {
-                if (!pathViejo && memoria.img == '') {
-                pathViejo = "../uploads/memoria/" + memoria.img;
-                borrarImg(pathViejo)
-                memoria.img = nombreArchivo;
-                await memoria.save();
-                return res?.status(200).json({
-                  ok: true,
-                  mensaje: "Memoria actualizado con exito",
-                  memoria,
-                });
-              }else {
-                pathViejo = "./uploads/memoria/" + memoria.img;  // pathViejo de la imagen si el usuario ya tiene una guardada
-                borrarImg(pathViejo)
-                memoria.img = nombreArchivo;
-                await memoria.save();
-                return res?.status(200).json({
-                  ok: true,
-                  mensaje: "Memoria actualizado con exito",
-                  memoria,
-                });
-              }
+      case "memoria":
+        try {
+          const memoria = await Memorias.findById(id).exec();
+          if (memoria) {
+            if (!pathViejo && memoria.img == "") {
+              pathViejo = "../uploads/memoria/" + memoria.img;
+              borrarImg(pathViejo);
+              memoria.img = nombreArchivo;
+              await memoria.save();
+              return res?.status(200).json({
+                ok: true,
+                mensaje: "Memoria actualizado con exito",
+                memoria,
+              });
+            } else {
+              pathViejo = "./uploads/memoria/" + memoria.img; // pathViejo de la imagen si el usuario ya tiene una guardada
+              borrarImg(pathViejo);
+              memoria.img = nombreArchivo;
+              await memoria.save();
+              return res?.status(200).json({
+                ok: true,
+                mensaje: "Memoria actualizado con exito",
+                memoria,
+              });
             }
-          } catch (error) {
-            // console.log({ error });
-            return res?.json({
-              ok: false,
-              mensaje: `No existe un memoria con id ${id}`,
-            });
           }
-        break
+        } catch (error) {
+          // console.log({ error });
+          return res?.json({
+            ok: false,
+            mensaje: `No existe un memoria con id ${id}`,
+          });
+        }
+        break;
 
-        case "otros":
-          try {
-            const otros = await Otros.findById(id).exec();
-            if (otros) {
-                if (!pathViejo && otros.img == '') {
-                pathViejo = "../uploads/otros/" + otros.img;
-                borrarImg(pathViejo)
-                otros.img = nombreArchivo;
-                await otros.save();
-                return res?.status(200).json({
-                  ok: true,
-                  mensaje: "El producto actualizado con exito",
-                  otros,
-                });
-              }else {
-                pathViejo = "./uploads/otros/" + otros.img;  // pathViejo de la imagen si el usuario ya tiene una guardada
-                borrarImg(pathViejo)
-                otros.img = nombreArchivo;
-                await otros.save();
-                return res?.status(200).json({
-                  ok: true,
-                  mensaje: "El producto actualizado con exito",
-                  otros,
-                });
-              }
+      case "otros":
+        try {
+          const otros = await Otros.findById(id).exec();
+          if (otros) {
+            if (!pathViejo && otros.img == "") {
+              pathViejo = "../uploads/otros/" + otros.img;
+              borrarImg(pathViejo);
+              otros.img = nombreArchivo;
+              await otros.save();
+              return res?.status(200).json({
+                ok: true,
+                mensaje: "El producto actualizado con exito",
+                otros,
+              });
+            } else {
+              pathViejo = "./uploads/otros/" + otros.img; // pathViejo de la imagen si el usuario ya tiene una guardada
+              borrarImg(pathViejo);
+              otros.img = nombreArchivo;
+              await otros.save();
+              return res?.status(200).json({
+                ok: true,
+                mensaje: "El producto actualizado con exito",
+                otros,
+              });
             }
-          } catch (error) {
-            // console.log({ error });
-            return res?.json({
-              ok: false,
-              mensaje: `No existe El producto con el id  ${id}`,
-            });
           }
-        break
+        } catch (error) {
+          // console.log({ error });
+          return res?.json({
+            ok: false,
+            mensaje: `No existe El producto con el id  ${id}`,
+          });
+        }
+        break;
 
       default:
         console.log("default");
         break;
-  }
+    }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 };
 
-
-export const crearImgs = async (data: any, res?: Response  ) => {
+export const crearImgs = async (data: any, res?: Response) => {
   try {
-    
     // const ImgsCreated  = Imgs.create({imgs: data}).then((data)=>{
     //   console.log({data})
-    // })  
-    const ImgsCreated  = await Imgs.create({imgs: data})  
-      return res?.json({
-         ok: true,
-         mensaje: "Imagenes guardadas exitosamente",
-         ImgsCreated,
-      });
-
+    // })
+    const ImgsCreated = await Imgs.create({ imgs: data });
+    return res?.json({
+      ok: true,
+      mensaje: "Imagenes guardadas exitosamente",
+      ImgsCreated,
+    });
   } catch (error) {
-    console.log('Error catch ====> ', error)
+    console.log("Error catch ====> ", error);
     throw new Error(`error ====> ${error}`);
   }
-}; 
+};
